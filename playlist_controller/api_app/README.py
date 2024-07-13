f4ed873f52f4a405e9323f05513ae655fa0305f1
Neal L <rlawton.cs@gmail.com>
2024-07-12 20:54:14 -0700

    Installed babel, webpack, react, and material-ui.  Wrote the configuration files for babel and webpack.

---
+++ playlist_controller/api_app/README.py
@@ -0,0 +1,3 @@
+# This directory (api_app) is a django app created with 
+# django-admin startapp <app_name>.  This directory will be used for 
+# our backend.
---
+++ playlist_controller/frontend_app/README.py
@@ -0,0 +1,3 @@
+# This directory (frontend_app) is a django app created with 
+# django-admin startapp <app_name>.  This directory will be used for 
+# our frontend.
