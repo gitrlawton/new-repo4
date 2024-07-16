9d3fc84fe08984aa7fffaf609652a549c6dbabd5
Neal L <rlawton.cs@gmail.com>
2024-07-15 23:54:05 -0700

    Wrote the base styling for the site.  Created the RoomJoin, HomePage, and CreateRoom components.

--- playlist_controller/frontend_app/src/components/App.js
+++ playlist_controller/frontend_app/src/components/App.js
@@ -1,5 +1,10 @@
+// This project uses class-based components, not functional components.
+
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from './HomePage';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
 
 export default class App extends Component {
     constructor(props) {
@@ -7,7 +12,13 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React Code</h1>;
+        return (
+            <div>
+                <HomePage />
+                <RoomJoinPage />
+                <CreateRoomPage />
+            </div>
+        )
     }
 }
 
